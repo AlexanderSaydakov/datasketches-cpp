ab29ad8f662125aa1bf6cf33465d0d7e2fe31e45
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2023-07-07 04:31:24 +0000

    deploy: 8d7b1c14ec5f72bf09ea3931cc13dd81f78201d9

---
+++ docs/0.0.1/search/classes_a.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['theta_5fa_5fnot_5fb_5falloc_0',['theta_a_not_b_alloc',['../classdatasketches_1_1theta__a__not__b__alloc.html',1,'datasketches']]],
+  ['theta_5fbase_5fbuilder_1',['theta_base_builder',['../classdatasketches_1_1theta__base__builder.html',1,'datasketches']]],
+  ['theta_5fbase_5fbuilder_3c_20builder_2c_20a_20_3e_2',['theta_base_builder&lt; builder, A &gt;',['../classdatasketches_1_1theta__base__builder.html',1,'datasketches']]],
+  ['theta_5fbase_5fbuilder_3c_20builder_2c_20allocator_20_3e_3',['theta_base_builder&lt; builder, Allocator &gt;',['../classdatasketches_1_1theta__base__builder.html',1,'datasketches']]],
+  ['theta_5fintersection_5falloc_4',['theta_intersection_alloc',['../classdatasketches_1_1theta__intersection__alloc.html',1,'datasketches']]],
+  ['theta_5fsketch_5falloc_5',['theta_sketch_alloc',['../classdatasketches_1_1theta__sketch__alloc.html',1,'datasketches']]],
+  ['theta_5fsketch_5falloc_3c_20std_3a_3aallocator_3c_20uint64_5ft_20_3e_20_3e_6',['theta_sketch_alloc&lt; std::allocator&lt; uint64_t &gt; &gt;',['../classdatasketches_1_1theta__sketch__alloc.html',1,'datasketches']]],
+  ['theta_5funion_5falloc_7',['theta_union_alloc',['../classdatasketches_1_1theta__union__alloc.html',1,'datasketches']]],
+  ['tuple_5fbase_5fbuilder_8',['tuple_base_builder',['../classdatasketches_1_1tuple__base__builder.html',1,'datasketches']]],
+  ['tuple_5fbase_5fbuilder_3c_20builder_2c_20array_5fof_5fdoubles_5funion_5fpolicy_5falloc_3c_20allocator_20_3e_2c_20allocator_20_3e_9',['tuple_base_builder&lt; builder, array_of_doubles_union_policy_alloc&lt; Allocator &gt;, Allocator &gt;',['../classdatasketches_1_1tuple__base__builder.html',1,'datasketches']]],
+  ['tuple_5fbase_5fbuilder_3c_20builder_2c_20array_5fof_5fdoubles_5fupdate_5fpolicy_3c_20a_20_3e_2c_20a_20_3e_10',['tuple_base_builder&lt; builder, array_of_doubles_update_policy&lt; A &gt;, A &gt;',['../classdatasketches_1_1tuple__base__builder.html',1,'datasketches']]],
+  ['tuple_5fbase_5fbuilder_3c_20builder_2c_20p_2c_20a_20_3e_11',['tuple_base_builder&lt; builder, P, A &gt;',['../classdatasketches_1_1tuple__base__builder.html',1,'datasketches']]],
+  ['tuple_5fintersection_12',['tuple_intersection',['../classdatasketches_1_1tuple__intersection.html',1,'datasketches']]],
+  ['tuple_5fintersection_3c_20aod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_2c_20policy_2c_20allocaod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_20_3e_13',['tuple_intersection&lt; aod&lt; std::allocator&lt; double &gt; &gt;, Policy, AllocAOD&lt; std::allocator&lt; double &gt; &gt; &gt;',['../classdatasketches_1_1tuple__intersection.html',1,'datasketches']]],
+  ['tuple_5fsketch_14',['tuple_sketch',['../classdatasketches_1_1tuple__sketch.html',1,'datasketches']]],
+  ['tuple_5fsketch_3c_20aod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_2c_20allocaod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_20_3e_15',['tuple_sketch&lt; aod&lt; std::allocator&lt; double &gt; &gt;, AllocAOD&lt; std::allocator&lt; double &gt; &gt; &gt;',['../classdatasketches_1_1tuple__sketch.html',1,'datasketches']]],
+  ['tuple_5fsketch_3c_20summary_2c_20std_3a_3aallocator_3c_20summary_20_3e_20_3e_16',['tuple_sketch&lt; Summary, std::allocator&lt; Summary &gt; &gt;',['../classdatasketches_1_1tuple__sketch.html',1,'datasketches']]],
+  ['tuple_5funion_17',['tuple_union',['../classdatasketches_1_1tuple__union.html',1,'datasketches']]],
+  ['tuple_5funion_3c_20aod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_2c_20array_5fof_5fdoubles_5funion_5fpolicy_5falloc_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_2c_20allocaod_3c_20std_3a_3aallocator_3c_20double_20_3e_20_3e_20_3e_18',['tuple_union&lt; aod&lt; std::allocator&lt; double &gt; &gt;, array_of_doubles_union_policy_alloc&lt; std::allocator&lt; double &gt; &gt;, AllocAOD&lt; std::allocator&lt; double &gt; &gt; &gt;',['../classdatasketches_1_1tuple__union.html',1,'datasketches']]]
+];
